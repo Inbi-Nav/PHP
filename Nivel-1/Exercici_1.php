fa0a5b3a8d65211e4b101aab3f76423c0f33cfcc
Inbisat Naveed <inkyn2002@gmail.com>
2025-11-01 14:57:57 +0100

    first commit

---
+++ Nivel-1/Exercici_1.php
@@ -0,0 +1,12 @@
+<h3 style="color: brown;">Exercici 1 </h3>
+
+<?php
+$evenNumbers = ['2', '4', '6', '8', '10', '12'];
+
+foreach ($evenNumbers as $numbers):?>
+
+   <p><?= "$numbers";?></p>
+
+<?php endforeach; ?>
+
+
